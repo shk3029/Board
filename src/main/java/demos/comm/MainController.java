67827d379abf808f3d9d573ddaa80161591ec5bf
BuildTools <unconfigured@null.spigotmc.org>
2018-02-13 17:29:59 +0900

    version 1.0

---
+++ src/main/java/demos/comm/MainController.java
@@ -0,0 +1,24 @@
+package demos.comm;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String Index(Model model) throws Exception {
+        System.out.println("index");
+        return "home";
+    }
+
+    @RequestMapping("/main")
+    public String Main(Model model) throws Exception {
+        System.out.println("main");
+        return "home";
+
+    }
+
+}
